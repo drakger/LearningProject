82cef5d21a1b9e38f5819ad2e853e374e84c1ead
Rákóczy Gergő <rakoczygergo@gmail.com>
2018-01-15 21:33:09 +0100

    Password Generator v1.0

--- Python/venv/main.py
+++ Python/venv/main.py
@@ -1,51 +1,58 @@
 import random
 import sys
 import os
+
+
+def get_y_n(message):
+    while True:
+        return_value = input(message+" y/n").lower()
+        if return_value == "y" or return_value == "n":
+            break
+        else:
+            print("Wrong input! Please enter y or n: ")
+    return return_value
+
+
+def get_password_info():
+    while True:
+        try:
+            length = int(input("How long your password should be? "))
+            break
+        except ValueError:
+            print("Not an integer!")
+    capital = get_y_n("Should it contain uppercase letters too?")
+    numbers = get_y_n("Should it contain numbers?")
+    special = get_y_n("Should it contain special characters?")
+
+    return_value = (length, capital, numbers, special)
+    return return_value
+
+
 def main():
     used_characters="abcdefghijklmnopqrstuvwxyz"
     uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     num="0123456789"
     spec="_-*@:!,+-=()[]{}#&"
     password=""
+
     while True:
-        while True:
-            try:
-                length=int(input("How long your password should be? "))
-                while True:
-                    capital=input("Should it contain uppercase letters too? y/n").lower()
-                    if capital=="y" or capital=="n":
-                        while True:
-                            numbers = input("Should it contain numbers? y/n").lower()
-                            if numbers== "y" or numbers == "n":
-                                while True:
-                                    special= input("Should it contain special characters? y/n").lower()
-                                    if special == "y" or special == "n":
-                                        break
-                                    else:
-                                        print("Wrong input!")
-                                break
-                            else:
-                                print("Wrong input!")
-                        break
-                    else:
-                        print("Wrong input!")
-                break
-            except ValueError:
-                print("Not an integer!")
-        if capital=="y":
+        password_info = get_password_info()
+        if password_info[1] == "y":
             used_characters+=uppercase
-        if numbers=="y":
-            used_characters+=num
-        if special=="y":
-            used_characters+=spec
-        length_of_characters=len(used_characters)
-        for x in range(1, length+1):
-            password+=used_characters[random.randint(0,length_of_characters-1)]
+        if password_info[2] == "y":
+            used_characters += num
+        if password_info[3] == "y":
+            used_characters += spec
+        length_of_characters = len(used_characters)
+        for x in range(1, password_info[0] + 1):
+            password += used_characters[random.randint(0,
+                                        length_of_characters - 1)]
         print(password)
-        ex=input("Would you like to generate another password? y/n")
-        if ex=="n":
+        ex = input("Would you like to generate another password? y/n")
+        if ex == "n":
             break
-        password="";
+        password = "";
         used_characters = "abcdefghijklmnopqrstuvwxyz"
+
 
 main()
